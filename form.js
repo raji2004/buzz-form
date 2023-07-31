059514bfb598e11dccd2c45b22af89063146c104
raji <kingraj1344@gmail.com>
2023-07-31 10:26:39 +0100

    first commit

---
+++ form.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form')
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData)
+
+    console.log(data)
+    // return sendData(data)
+
+
+
+})
+async function sendData(data) {
+    try {
+
+        const res = await axios.post('https://buzzbackend1.vercel.app/deleteaccount', { ...data })
+        console.log(res)
+        alert("sorry for not meeting your expectations")
+       
+       
+
+
+        return res
+    } catch (error) {
+        console.log(error.response)
+        return error.response
+    }
+}
+// async function sendMail(data) {
+//     const res = await axios.put('https://server-flax-pi.vercel.app', { ...data })
+//     return res
+// }
